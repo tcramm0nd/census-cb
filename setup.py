64950c0840946a6c2da9c0bd7ad7177c42b6936b
tcramm0nd <25987791+tcramm0nd@users.noreply.github.com>
2021-09-06 21:31:27 -0400

    Changed file structure for src/

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="census-cb",
+    version="0.0.3",
+    author="Tim Crammond",
+    author_email="author@example.com",
+    description="Wrapper for downloading and processing Cartography Boundaries from the US Census Bureau",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tcramm0nd/census-cb",
+    project_urls={
+        "Bug Tracker": "https://github.com/tcramm0nd/census-cb/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    python_requires=">=3.6",
+    install_requires=['geopandas==0.9.0',
+                      'pandas==1.3.2',
+                      'requests==2.26.0']
+)
